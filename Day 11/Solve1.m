5ee9455fc8199c5d3ac7a9f9bbbdb5c550937541
Sergei Chekmarev <slimercorp@gmail.com>
2021-12-11 12:08:28 +0300

    Day 11

---
+++ Day 11/Solve1.m
@@ -0,0 +1,110 @@
+clear; clc;
+fid = fopen('input.txt');
+sizee = 10;
+map = zeros(sizee,sizee);
+row = 0;
+while 1
+    tline = fgetl(fid);
+    if ~ischar(tline)
+        break;
+    end
+    
+    row = row + 1;
+    for i=1:length(tline)
+        map(row,i) = str2num(tline(i));
+    end
+    
+end
+
+%%
+step = 100;
+flashes = 0;
+for k=1:step
+    flashMap = zeros(sizee,sizee);
+    map = map + 1;
+    %search >9
+    thereWasFlashing = true;
+    while thereWasFlashing
+        thereWasFlashing = false;
+        for i=1:sizee
+            for j=1:sizee
+                if map(i,j) > 9 && flashMap(i,j) ~= 1
+                    map = transferEnergy(map,i,j, sizee);
+                    map(i,j) = map(i,j) + 1;
+                    flashMap(i,j) = 1;
+                    thereWasFlashing = true;
+                end
+            end
+        end
+    end
+
+    %clean
+    for i=1:sizee
+        for j=1:sizee
+            if map(i,j) > 9
+                map(i,j) = 0;
+            end
+        end
+    end
+    
+    flashes = flashes + sum(sum(flashMap));
+end
+flashes 
+
+function [map] = transferEnergy(map,i,j, sizee)
+    %up-left
+    row = i - 1;
+    column = j -1;
+    if row > 0 && column > 0
+        map(row,column) = map(row,column) + 1;
+    end
+    
+    % up
+    row = i - 1;
+    column = j;
+    if row > 0
+        map(row,column) = map(row,column) + 1;
+    end
+    
+    % up-right
+    row = i - 1;
+    column = j + 1;
+    if row > 0 && column <=sizee
+        map(row,column) = map(row,column) + 1;
+    end
+    
+   % left
+    row = i;
+    column = j - 1;
+    if column > 0
+        map(row,column) = map(row,column) + 1;
+    end
+    
+    % right
+    row = i;
+    column = j + 1;
+    if column <=sizee
+        map(row,column) = map(row,column) + 1;
+    end
+     
+    % down-left
+    row = i+1;
+    column = j - 1;
+    if row <= sizee && column >0
+        map(row,column) = map(row,column) + 1;
+    end
+    
+    % down
+    row = i+1;
+    column = j;
+    if row <= sizee
+        map(row,column) = map(row,column) + 1;
+    end
+    
+    % down-right
+    row = i+1;
+    column = j + 1;
+    if row <= sizee && column <= sizee
+        map(row,column) = map(row,column) + 1;
+    end    
+end
