afdeb00e0d0313a12d3a3603cc8f6f56f4b8448f
Sergei Chekmarev <slimercorp@gmail.com>
2021-12-13 10:48:14 +0300

    Day 13

---
+++ Day 13/Solve2.m
@@ -0,0 +1,73 @@
+clear; clc;
+fid = fopen('inputPart1.txt');
+map = zeros(10,10);
+while 1
+    tline = fgetl(fid);
+    if ~ischar(tline)
+        break;
+    end
+
+    indexs = find(tline == ',');
+    x = str2num(tline(1:indexs(1)-1))+1;
+    y = str2num(tline(indexs(1)+1:end))+1;
+
+    map(y,x) = 1;
+end
+
+fid2 = fopen('inputPart2.txt');
+instruction = [0 0];
+row = 0;
+while 1
+    tline = fgetl(fid2);
+    if ~ischar(tline)
+        break;
+    end
+    row = row + 1;
+    indexs = find(tline == '=');
+    axis = tline(1:indexs(1)-1);
+    value = str2num(tline(indexs(1)+1:end))+1;
+
+    if axis == 'x'
+        axisN = 1;
+    else
+        axisN = 0;
+    end
+    instruction(row,1) = axisN;
+    instruction(row,2) = value;
+end
+
+%%
+for i=1:12
+    axis = instruction(i,1);
+    value = instruction(i,2);
+
+    if axis %x
+        [row,column] = size(map);
+        newMap = zeros(row, value);
+        newMap(1:row,1:value) = map(1:row,1:value);
+        for k=value+1:column
+            for j=1:row
+                column2 = 2*value - k;
+                if column2 ==0
+                    break;
+                end
+                newMap(j,column2) = min(newMap(j,column2) + map(j,k), 1);
+            end
+        end
+        map = newMap;
+    else %y
+        [row,column] = size(map);
+        newMap = zeros(value, column);
+        newMap(1:value,1:column) = map(1:value,1:column);
+        for k=value+1:row
+            for j=1:column
+                row2 = 2*value - k;
+                if row2 ==0
+                    break;
+                end
+                newMap(row2,j) = min(newMap(row2,j) + map(k,j), 1);
+            end
+        end
+        map = newMap;
+    end
+end
